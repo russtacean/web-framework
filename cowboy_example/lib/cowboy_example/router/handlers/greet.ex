aaaee9bf24efdbcb73463074371c52f1d6950923
Russ Gunther <russell.g@leantaas.com>
2024-08-21 10:41:39 -0400

    Chap 1: Validate HTTP method so only get succeeds, otherwise 404

--- cowboy_example/lib/cowboy_example/router/handlers/greet.ex
+++ cowboy_example/lib/cowboy_example/router/handlers/greet.ex
@@ -10,7 +10,7 @@ defmodule CowboyExample.Router.Handlers.Greet do
   This function handles the "/greet/:who", logs the
   requests and responds with Hello `who` as the body
   """
-  def init(req0, state) do
+  def init(%{method: "GET"} = req0, state) do
     Logger.info("Received request: #{inspect(req0)}")
     who = :cowboy_req.binding(:who, req0)
 
@@ -25,6 +25,20 @@ defmodule CowboyExample.Router.Handlers.Greet do
         200,
         %{"content-type" => "text/html"},
         "#{greeting} #{who}",
+        req0
+      )
+
+    {:ok, req1, state}
+  end
+
+  def init(req0, state) do
+    Logger.info("Received request: #{inspect(req0)}")
+
+    req1 =
+      :cowboy_req.reply(
+        404,
+        %{"content-type" => "text/html"},
+        "404 Not found",
         req0
       )
 
